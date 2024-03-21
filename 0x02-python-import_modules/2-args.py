b220dbf219655e1416c928b89db0346e066fbac8
Josephblaq <Josephforgod11@gmail.com>
2024-03-20 22:21:42 -0700

    2-args.py update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments: ".format(args_numbs))
     for o in range(args_numbs):
-        print("{}: {:s}".format(o+1, arguments[o]))
+        print("{}: {}".format(o+1, arguments[o]))
