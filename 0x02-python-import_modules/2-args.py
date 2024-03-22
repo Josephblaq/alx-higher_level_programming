b0ebadbd1b2c220a84228fe134eaa006292afbe5
Josephblaq <Josephforgod11@gmail.com>
2024-03-21 19:59:22 -0700

    2-args.py update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     args_numbs = len(arguments)
     if args_numbs == 0:
-        print("0 arguments.")
+        print("0 arguments.\n")
     elif args_numbs == 1:
-        print("{} argument:".format(args_numbs))
+        print("{} argument:\n".format(args_numbs))
     else:
-        print("{} arguments: ".format(args_numbs))
+        print("{} arguments:\n".format(args_numbs))
     for o in range(args_numbs):
-        print("{}: {}".format(o+1, arguments[o]))
+        print("{}: {}\n ".format(o+1, arguments[o]))
